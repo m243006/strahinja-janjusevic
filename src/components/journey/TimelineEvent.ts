086e71c3dd3f91a33168811955a7940c386bb52d
Strahinja Janjusevic <89561041+m243006@users.noreply.github.com>
2025-01-28 18:10:54 -0500

    las

--- src/components/journey/TimelineEvent.ts
+++ src/components/journey/TimelineEvent.ts
@@ -14,7 +14,7 @@ export const timeline: TimelineEvent[] = [
     year: "2020-2024",
     title: "U.S. Naval Academy",
     description: "Bachelor of Science - Dual Major in Cyber Operations (NSA-CAE) and Computer Science",
-    details: "During my time at the United States Naval Academy, I pursued a dual major in Cyber Operations and Computer Science, graduating with a strong foundation in both technical expertise and leadership. As part of the prestigious NSA Centers of Academic Excellence (CAE) program, I honed my skills in cybersecurity, focusing on threat intelligence, network defense, and operational methodologies. My capstone project introduced an innovative approach to tracking threat actors, leveraging reputation-based systems to enhance network intrusion detection and attribution. Beyond academics, I served in leadership roles as a Squad Leader and Drill Officer, mentoring peers and ensuring the success of over 140 midshipmen. These experiences not only deepened my technical knowledge but also instilled a strong sense of discipline, teamwork, and commitment to excellence—qualities that continue to drive my work in cybersecurity and artificial intelligence.",
+    details: "During my time at the United States Naval Academy, I pursued a dual major in Cyber Operations and Computer Science, graduating with a strong foundation in both technical expertise and leadership. As part of the prestigious NSA Centers of Academic Excellence (CAE) program, I honed my skills in cybersecurity, focusing on threat intelligence, network defense, and operational methodologies. Beyond academics, I was a part of a team that competed at the University of Maryland Data Challenge, where we secured the award for Outstanding Cybersecurity Project by enhancing code and infrastructure to create a more secure environment. Additionally, I served in leadership roles as a Squad Leader and Drill Officer, mentoring peers and ensuring the success of over 140 midshipmen. These experiences instilled a strong sense of discipline and teamwork.",
     delay: 0.1,
     position: { x: 15, y: 5 },
     bgImage: "/public/lovable-uploads/13468a15-c480-49f5-a07a-ff29333dd9f7.png",
@@ -50,7 +50,7 @@ export const timeline: TimelineEvent[] = [
     ]
   },
   {
-    year: "2023",
+    year: "2024",
     title: "Capstone Project",
     description: "Threat Intelligence and Behaviour Analysis Research",
     details: "For my capstone project at the United States Naval Academy, We developed an innovative methodology to track and attribute threat actors by leveraging IP reputation. This approach enabled the systematic harvesting of threat intelligence data, allowing for the identification of new targets and maintaining continuity on known adversaries by detecting changes in their tradecraft. By focusing on reputation as a key metric, the project introduced a novel framework for network intrusion detection that not only enhanced threat visibility but also improved the ability to attribute malicious activities to specific actors.",
@@ -64,28 +64,31 @@ export const timeline: TimelineEvent[] = [
   },
   {
     year: "2024-Present",
-    title: "MIT Graduate Research",
+    title: "MIT Technology and Policy",
     description: "Master's in Technology and Policy",
     details: "As a graduate student in the Technology and Policy Program (TPP) at the Massachusetts Institute of Technology (MIT), I am exploring the critical intersection of technology, policy, and society, with a focus on artificial intelligence and cybersecurity. This interdisciplinary program equips me with the tools to analyze complex technological challenges and develop policy solutions that balance innovation, security, and ethical considerations. My research and insights have also extended to the global stage, as demonstrated by my keynote address at the Philippine Navy’s Cybersecurity Awareness Webinar, where I discussed the role of AI in modern military operations and its implications for international security. Through TPP, I am deepening my understanding of how emerging technologies can address global challenges while ensuring responsible governance, empowering me to contribute to impactful decisions that shape the future of technology in a rapidly evolving world.",
     delay: 0.5,
     position: { x: 15, y: 80 },
     bgImage: "photo-1466721591366-2d5fba72006d",
     expandedImages: [
+      "/lovable-uploads/crnog.PNG",
       "/lovable-uploads/Certificate.jpg",
       "/lovable-uploads/Sched.jpg",
+      "/lovable-uploads/Tpp.PNG",
+      
     ]
   },
   {
     year: "2024-Present",
-    title: "MIT ALFA Group",
+    title: "MIT CSAIL",
     description: "Artificial Adversarial Intelligence Research",
     details: "My research focuses on advancing the intersection of artificial intelligence and cybersecurity, with a particular emphasis on adversarial intelligence and threat detection. At MIT’s Computer Science and Artificial Intelligence Laboratory (CSAIL), I am working with the Anyscale Learning For All (ALFA) group to computationally replicate adversarial behaviors, exploring how AI agents can compete, learn, and adapt in simulated environments like CybORG. Leveraging Large Language Models (LLMs) and the BRON knowledge graph, ALFA is developing coevolutionary algorithms to enhance the capabilities of AI-driven cybersecurity systems.",
     delay: 0.6,
     position: { x: 30, y: 95 },
     bgImage: "photo-1485827404703-89b55fcc595e",
     expandedImages: [
-      "photo-1485827404703-89b55fcc595e",
-      "photo-1451187580459-43490279c0fa",
+      "/lovable-uploads/Mit.jpg",
+      "/lovable-uploads/csail.jpg",
     ]
   }
 ];
